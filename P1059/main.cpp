3deeeab3d6926cb40df5845fd4861ff536d2fd11
HugeSingularity <kqjiang1111@gmail.com>
2024-02-25 11:33:50 +0800

    Data: 2024/2/25

---
+++ P1059/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <cstdio>
+
+short number[1001];
+
+int main()
+{
+    int n, m = 0;
+    scanf("%d", &n);
+    while (n--)
+    {
+        int tmd;
+        scanf("%d", &tmd);
+        if (!number[tmd])
+        {
+            m++;
+            number[tmd] = tmd;
+        }
+    }
+    printf("%d\n", m);
+    for (int i = 1; i <= 1000; i++)
+        if (number[i]) printf("%d ", number[i]);
+    return 0;
+}
