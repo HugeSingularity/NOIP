62226e8a9ca55fe11613f866a2c3f1984164c35d
LFTEC <jcpuff20@gmail.com>
2024-02-28 16:27:46 +0800

    date: 2024-2-28 P1093

---
+++ P1093/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <cstdio>
+
+struct student
+{
+    int a, b, c, together, number = 0;
+    student(int a = 0, int b = 0, int c = 0, int num = 0) : a(a), b(b), c(c), number(num), together(a + b + c) {}
+
+};
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    student m[n];
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        m[i] = student(a, b, c, i);
+    }
+    for (int i = 0; i < 5; i++)
+        for (int j = i + 1; j < n; j++)
+        {
+            if (m[i].together < m[j].together || m[i].together == m[j].together && (m[i].a < m[j].a || m[i].a == m[j].a && m[i].number > m[j].number))
+            {
+                student tmp = m[i];
+                m[i] = m[j];
+                m[j] = tmp;
+            }
+        }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d %d\n", m[i].number+1, m[i].together);
+    }
+    return 0;
+}
