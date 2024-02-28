6a04b96d3ec9d49229a2693de8b5936a9cf9199f
姜耀 <ybjy82@163.com>
2024-02-28 20:01:01 +0800

    date: 2024-2-28 P1781

---
+++ P1781/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <cstdio>
+#include <cstring>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char* a = new char[100];
+    
+    int number = 1;
+
+    scanf("%s", a);
+    
+    for (int i = 2; i <= n; i++)
+    {
+        char* b = new char[100];
+
+        scanf("%s", b);
+        if (strlen(a) < strlen(b))
+        {
+            number = i;
+            a = b;
+        }
+        else if (strlen(a) == strlen(b))
+        {
+            for (int j = 0; j < strlen(b); j++)
+                if (a[j] < b[j])
+                {
+                    number = i;
+                    a = b;
+                    break;
+                }
+                else if (a[j] > b[j])
+                    break;
+        }
+    }
+    printf("%d\n", number);
+    printf("%s", a);
+    return 0;
+}
